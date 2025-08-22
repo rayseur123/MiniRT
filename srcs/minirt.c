e2a4d668ded8399e86fd789a98ef5b85e383ec7f
Nicolas Jooris <njooris@student.42lyon.fr>
2025-08-22 13:08:41 +0200

    test gitaction

--- srcs/minirt.c
+++ srcs/minirt.c
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 int	main()
 {
 	int i = 0;
 	return (1);
 }
-=======
-#include <stdio.h>
-int main(void)
-{
-	printf("TEST CHECK !\n");
-	return (1);
-}
->>>>>>> 3884ac638f56706548adb3e8aa74ff735e347def
