8b351ced411ab7edac2c992168b6d372f30129ec
Nicolas Jooris <njooris@student.42lyon.fr>
2025-08-22 11:49:55 +0200

    PATCH : lst_get_prev : patch for the norme

--- libs/libft/lst/ft_lstgetprev.c
+++ libs/libft/lst/ft_lstgetprev.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstgetprev.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: njooris <njooris@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:59:37 by lilefebv          #+#    #+#             */
-/*   Updated: 2024/11/23 15:52:50 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2025/08/22 11:48:35 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 t_list	*ft_lstgetprev(t_list *first, t_list *actual)
 {
-    t_list	*temp;
+	t_list	*temp;
 
-    if (first == NULL || actual == NULL || first == actual)
-        return (NULL);
-    temp = first;
-    while (temp->next)
-    {
-        if (temp->next == actual)
-            return (temp);
-        temp = temp->next;
-    }
-    return (NULL);
+	if (first == NULL || actual == NULL || first == actual)
+		return (NULL);
+	temp = first;
+	while (temp->next)
+	{
+		if (temp->next == actual)
+			return (temp);
+		temp = temp->next;
+	}
+	return (NULL);
 }
