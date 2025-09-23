8a4add9ac9fe02349e41135a58d749f5902ba01e
Nicolas Jooris <njooris@z1r5p4.42lyon.fr>
2025-09-23 11:35:14 +0200

    ADD add translation tuple : do a translation to a tuple with translation func

--- srcs/manage_scene/transformation/translation.c
+++ srcs/manage_scene/transformation/translation.c
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:22:28 by njooris           #+#    #+#             */
-/*   Updated: 2025/09/22 16:04:08 by njooris          ###   ########.fr       */
+/*   Updated: 2025/09/23 11:33:52 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,11 @@ t_matrix4 *translation(double x, double y, double z, t_matrix4 m1)
 	m1[1][3] = y;
 	m1[2][3] = z;
 	return (m1);
+}
+
+t_tuple	translation_tuple(double x, double y, double z, t_tuple point)
+{
+	t_matrix4	matrix;
+
+	return (matrix4_multiplication_by_tuple(translation(x, y, z, matrix), point));
 }
