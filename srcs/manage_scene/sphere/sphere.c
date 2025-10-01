f757ecee7d520d4a91a5667eb39295a058235525
Nicolas Jooris <njooris@z1r7p4.42lyon.fr>
2025-10-01 13:31:45 +0200

    norme

--- srcs/manage_scene/sphere/sphere.c
+++ srcs/manage_scene/sphere/sphere.c
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 10:14:57 by njooris           #+#    #+#             */
-/*   Updated: 2025/10/01 13:10:30 by njooris          ###   ########.fr       */
+/*   Updated: 2025/10/01 13:31:26 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,16 +51,15 @@ void	set_transform(t_obj *s, t_matrix4 m)
 	matrix4_cpy(s->transform, m);
 }
 
-void	do_draw_sphere(t_canvas canvas, double wall_size, t_rgb color, t_obj *obj)
+void	do_draw_sphere(t_canvas canvas, double wall_size,
+			t_rgb color, t_obj *obj)
 {
 	u_int32_t	x;
 	u_int32_t	y;
-	double		pixel_size;
 	double		half;
 	t_inters	xs;
 	t_ray		ray;
 
-	pixel_size = wall_size / WIDTH_CANVA;
 	half = wall_size / 2;
 	ray.origin = set_point(0, 0, -5);
 	x = -1;
@@ -69,11 +68,13 @@ void	do_draw_sphere(t_canvas canvas, double wall_size, t_rgb color, t_obj *obj)
 		y = -1;
 		while (++y < HEIGHT_CANVA)
 		{
-			t_tuple position = set_point(-half + pixel_size * x, half - pixel_size * y, 10);
-			ray.direction = tuple_normalization(tuple_subtraction(position, ray.origin));
-			intersect_sphere(obj, ray, &xs);
+			ray.direction = tuple_normalization(tuple_subtraction(
+						set_point(-half + (wall_size / WIDTH_CANVA) * x,
+							half - (wall_size / WIDTH_CANVA) * y, 10),
+						ray.origin));
+			intersect(ray, obj, &xs);
 			if (hit(&xs))
-				put_px_in_canva(canvas, x ,y , color);
+				put_px_in_canva(canvas, x, y, color);
 		}
 	}
 }
@@ -81,9 +82,8 @@ void	do_draw_sphere(t_canvas canvas, double wall_size, t_rgb color, t_obj *obj)
 void	draw_sphere(t_canvas canvas)
 {
 	t_obj		obj;
-	double wall_size;
-	
-	
+	double		wall_size;
+
 	wall_size = 10;
 	obj = sphere();
 	do_draw_sphere(canvas, wall_size, set_rgb(255, 0, 0), &obj);
