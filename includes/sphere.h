602d40b9bc4cceedbd64c24726c393e933200937
Nicolas Jooris <njooris@z1r7p3.42lyon.fr>
2025-11-12 11:31:45 +0100

    patch headers

--- includes/sphere.h
+++ includes/sphere.h
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 08:42:14 by dernst            #+#    #+#             */
-/*   Updated: 2025/10/22 14:06:50 by njooris          ###   ########.fr       */
+/*   Updated: 2025/11/12 10:20:35 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 #include "intersection.h"
 
 uint32_t 		intersect_sphere(t_obj *s, t_ray r, t_inters *inters);
-t_obj			sphere(void);
 
 #endif
 
--- includes/transform.h
+++ includes/transform.h
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 08:41:28 by dernst            #+#    #+#             */
-/*   Updated: 2025/10/24 15:15:13 by njooris          ###   ########.fr       */
+/*   Updated: 2025/11/12 10:20:24 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,5 @@ t_matrix4_ptr	scaling(double x, double y, double z, t_matrix4 m);
 t_matrix4_ptr	rotation_x(double radian, t_matrix4 m);
 t_matrix4_ptr	rotation_y(double radian, t_matrix4 m);
 t_matrix4_ptr	rotation_z(double radian, t_matrix4 m);
-void			set_transform(t_obj *s, t_matrix4 m);
 
 #endif //MINIRT_TRANSFORM_H
